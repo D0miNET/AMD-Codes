6defbfb2d76601ca6c19dfc474e6dbdced8cba1b
Domi_NET <lix6727cristal@gmail.com>
2025-07-05 12:42:55 +0300

    Add files via upload

---
+++ Lectia 1 - First Baze.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout<<"Vlad de la AMD e cel mai bun profesor :)"<<std::endl;
+    std::cout<<"<3 Vlad"<<std::endl;
+    return 0;
+
+}
+
+/*
+
+using namespace std; // pentru a ne referi mai simplu la indeficatorii spatiali + acesta face ca std să fie accesibil direct.
+
+int main() // functie - set de instructiuni
+
+std::cout << "Hello world";
+std:: <- (STandraD Charcter OUTput) - dizpozitiv standard de iesire
+<< <- operator insertie (insert)
+return 0; - marcheaza finalul executiei functiei main si a programului nostru
+
+Valoarea 0 - semnficia ca programul nostru s-a incheiat cu succes (Reprezinta finalul functie main)
+Orice instructiune dupa return 0; nu va mai fi executata
+cout, endl - identificatoare de nume std
+
+
+
+*/
+
+/* comentarii de tip bloc */
+
+// comentarii de tip linie
